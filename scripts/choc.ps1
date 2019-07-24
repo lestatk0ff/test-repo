05c4c65b138595c40f919498a23b54f930f65b51
Anton Kaminski <anton_kaminski@epam.com>
2019-07-24 16:45:43 +0300

    TEST2

--- scripts/choc.ps1
+++ scripts/choc.ps1
@@ -1,7 +1,8 @@
 # Install Chocolatey
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+#iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
 # Globally Auto confirm every action
-choco feature enable -n allowGlobalConfirmation
+#choco feature enable -n allowGlobalConfirmation
 
 # Install Python 2.x 2.7.15
 #choco install python2
