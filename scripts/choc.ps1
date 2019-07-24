8a8da02792b4de026e37b9805f6cb27245f761f1
Anton Kaminski <anton_kaminski@epam.com>
2019-07-24 16:10:28 +0300

    added choko

--- scripts/choc.ps1
+++ scripts/choc.ps1
@@ -4,4 +4,7 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/in
 choco feature enable -n allowGlobalConfirmation
 
 # Install Python 2.x 2.7.15
-choco install python2
+#choco install python2
+
+# Install Python 3.x
+#choco install python
